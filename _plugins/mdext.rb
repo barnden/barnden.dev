3f0b00fa4782a2a1085938579cda729c22addb80
barnden <brandon@barnden.dev>
2021-02-08 14:36:31 -0600

    add remark block and styling

--- _plugins/mdext.rb
+++ _plugins/mdext.rb
@@ -19,17 +19,17 @@ module Jekyll
         "<div class='block example' markdown='1'>#{content}</div>"
       end
     end
-  class EquationBlock < Liquid::Block
+  class RemarkBlock < Liquid::Block
       def initialize(tag_name, text, tokens)
         super
       end
       require "kramdown"
       def render(context)
         content = super
-        "<div class='block equation' markdown='1'>#{content}</div>"
+        "<div class='block remark' markdown='1'>#{content}</div>"
       end
     end
   end
   Liquid::Template.register_tag('definition', Jekyll::DefinitionBlock)
   Liquid::Template.register_tag('example', Jekyll::ExampleBlock)
-  Liquid::Template.register_tag('equation', Jekyll::EquationBlock)
+  Liquid::Template.register_tag('remark', Jekyll::RemarkBlock)
