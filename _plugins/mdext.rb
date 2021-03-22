fef17a54fb474f3e437bf409ba60a2b38edd7a75
barnden <brandon@barnden.dev>
2021-03-22 00:13:52 -0500

    Update block plugin

--- _plugins/mdext.rb
+++ _plugins/mdext.rb
@@ -29,7 +29,29 @@ module Jekyll
         "<div class='block remark' markdown='1'>#{content}</div>"
       end
     end
+  class ProofBlock < Liquid::Block
+      def initialize(tag_name, text, tokens)
+        super
+      end
+      require "kramdown"
+      def render(context)
+        content = super
+        "<div class='block proof' markdown='1'>#{content}</div>"
+      end
+    end
+  class TheoremBlock < Liquid::Block
+      def initialize(tag_name, text, tokens)
+        super
+      end
+      require "kramdown"
+      def render(context)
+        content = super
+        "<div class='block theorem' markdown='1'>#{content}</div>"
+      end
+    end
   end
   Liquid::Template.register_tag('definition', Jekyll::DefinitionBlock)
   Liquid::Template.register_tag('example', Jekyll::ExampleBlock)
   Liquid::Template.register_tag('remark', Jekyll::RemarkBlock)
+  Liquid::Template.register_tag('proof', Jekyll::ProofBlock)
+  Liquid::Template.register_tag('theorem', Jekyll::TheoremBlock)
